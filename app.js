6b7d3c43d35fe64982a6094050bd757b8aabc4bd
Blake-Yoder <bdogyoder@gmail.com>
2021-05-21 16:04:11 -0400

    initial backend commit

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//APP RUNNING ON PORT 5000
+
 var app = express();
+
+//MONGO CONNECTION
+//ask bob about this section
+var connectionString = "mongo"mongodb+srv://warroom.byzmr.mongodb.net/myFirstDatabase" --username dbuser"
+const {MongoClient} = require('mongodb');
 
 app.use(logger('dev'));
 app.use(express.json());
