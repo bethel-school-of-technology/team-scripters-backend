e440f87dad54d5c656935e94315bf92ca76ecae1
Blake-Yoder <bdogyoder@gmail.com>
2021-06-06 21:13:37 -0400

    added post CRUD routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts')
 
 //APP RUNNING ON PORT 5000
 
@@ -31,5 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/posts', postsRouter);
 
 module.exports = app;
--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,91 @@
 var express = require('express');
 const { route } = require('.');
 var router = express.Router();
-var User = require('../models/users')
+var Post = require('../models/posts')
+
+
+//add a new post
+router.post('/posts', async (req,res,next) => {
+
+    try{
+      console.log(req.body)
+       let newPost = new Post({
+         title: req.body.title,
+         body: req.body.body,
+         published: req.body.published
+       });
+       
+       //console.log(newPost)
+       let result = await newPost.save();
+       //console.log(result);
+       res.json({
+         message: "Post created successfuly",
+         status: 200
+       })
+    }
+    catch(err){
+     console.log(err);
+     res.json({
+       message: "Post not created successfully",
+       status: 403,
+     })
+    }
+   })
+
+   //Get all posts 
+router.get('/posts', async (req,res,next) => {
+    
+    Post.find({
+        title: req.body.title,
+        body: req.body.body,
+        published: req.body.published
+    })
+    })  
+   
+    
+    
+//Get post by ID 
+router.get('/posts/:id', async (req,res,next) => {
+
+    Post.findById(req.params._id, (error, data) => {
+        if (error) {
+            return next(error)
+        } 
+        else {
+            res.json(data)
+        }
+    })
+
+})
+
+//Update post 
+router.put('/edit-post/:id', async (req,res,next) => {
+
+    Post.findByIdAndUpdate(req.params._id, {
+        $set: req.body
+    }, (error, data) => {
+        if (error) {
+            return next(error);
+            console.log(error)
+        } else {
+            res.json(data)
+            console.log('Post updated successfully')
+        }
+    })
+    })
+
+//Delete a book
+router.delete('/delete-post/:id', async (req,res,next) => {
+    
+    Post.findByIdAndRemove(req.params._id, (error, data) => {
+        if(error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                msg: data
+            })
+        }
+    })
+})
+
+module.exports = router;
