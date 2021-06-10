bcff2a12e8f070bedbd81765a22b6811e95ebd4b
Blake-Yoder <bdogyoder@gmail.com>
2021-06-09 20:18:41 -0400

    posts crud testing

--- routes/posts.js
+++ routes/posts.js
@@ -5,52 +5,49 @@ var Post = require('../models/posts')
 
 
 //add a new post
-router.post('/posts', async (req,res,next) => {
+router.post('/add-post', async (req, res, next) => {
 
-    try{
-      console.log(req.body)
-       let newPost = new Post({
-         title: req.body.title,
-         body: req.body.body,
-         published: req.body.published
-       });
-       
-       //console.log(newPost)
-       let result = await newPost.save();
-       //console.log(result);
-       res.json({
-         message: "Post created successfuly",
-         status: 200
-       })
-    }
-    catch(err){
-     console.log(err);
-     res.json({
-       message: "Post not created successfully",
-       status: 403,
-     })
-    }
-   })
+    try {
+        console.log(req.body)
+        let newPost = new Post({
+            title: req.body.title,
+            body: req.body.body,
+            published: req.body.published
+        });
 
-   //Get all posts 
-router.get('/posts', async (req,res,next) => {
-    
-    Post.find({
-        title: req.body.title,
-        body: req.body.body,
-        published: req.body.published
-    })
-    })  
-   
-    
-    
+        //console.log(newPost)
+        let result = await newPost.save();
+        //console.log(result);
+        res.json({
+            message: "Post created successfuly",
+            status: 200
+        })
+    }
+    catch (err) {
+        console.log(err);
+        res.json({
+            message: "Post not created successfully",
+            status: 403,
+        })
+    }
+})
+
+//Get all posts 
+router.get('/', async (req, res, next) => {
+
+   let posts = await Post.find()
+    res.json(posts)
+})
+
+
+
 //Get post by ID 
-router.get('/posts/:id', async (req,res,next) => {
+router.get('/:id', async (req, res, next) => {
 
-    Post.findById(req.params._id, (error, data) => {
+    Post.findById(req.params.id, (error, data) => {
         if (error) {
             return next(error)
-        } 
+        }
         else {
             res.json(data)
         }
@@ -59,9 +56,9 @@ router.get('/posts/:id', async (req,res,next) => {
 })
 
 //Update post 
-router.put('/edit-post/:id', async (req,res,next) => {
+router.put('/edit-post/:id', async (req, res, next) => {
 
-    Post.findByIdAndUpdate(req.params._id, {
+    Post.findByIdAndUpdate(req.params.id, {
         $set: req.body
     }, (error, data) => {
         if (error) {
@@ -72,13 +69,13 @@ router.put('/edit-post/:id', async (req,res,next) => {
             console.log('Post updated successfully')
         }
     })
-    })
+})
 
 //Delete a book
-router.delete('/delete-post/:id', async (req,res,next) => {
-    
-    Post.findByIdAndRemove(req.params._id, (error, data) => {
-        if(error) {
+router.delete('/delete-post/:id', async (req, res, next) => {
+
+    Post.findByIdAndRemove(req.params.id, (error, data) => {
+        if (error) {
             return next(error);
         } else {
             res.status(200).json({
